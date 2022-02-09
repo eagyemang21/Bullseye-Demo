7044f45c8f6efbf4a8182d0b73510056a0cbabd2
eagyemang21 <elijahagyemang9@gmail.com>
2022-02-09 15:27:17 -0500

    functions practice

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+//function for hit
+
+const hit = (shotLocation, computerPostition) => {
+    let HP = 300;
+    if(computerPosition === playerShotLocation){
+        return HP - 100;
+    }
+}
+const result = hitCounter =>{
+    if(hitCounter === 3){
+        return "Game Over"
+    }
+    //implement the button here for restart and refresh the game
+    //
+}
+
+
+//health counter obj 
+//the value is the number of shots the computer hits on you 
+//afterwards, we make it to where they 
+//health counter key 
+
+let healthPlayer1 /*(could be for player 1)*/ = {100}
+let healthComputer /*(would be for computer obj)*/ = {100}
 
